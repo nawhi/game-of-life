fef48849b416fc700375892101b775b562960bf3
Nick White <nawhi3@outlook.com>
2018-11-30 12:43:59 +0000

    Handle empty board size 1

--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GameOfLifeShould {
+    @Test
+    void not_change_empty_board_size_one() {
+        GameOfLife game = new GameOfLife();
+        int[][] cells = new int[][] { { 0 } };
+        assertThat(game.nextGen(cells), is(cells));
+    }
+
 }
