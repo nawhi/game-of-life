97f092c1f60a46fbfa2970016e7e28f0ab6655c5
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-14 17:13:52 +0000

    Delete superceded test

--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -5,11 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class GameShould {
 
     @Test
-    public void generate_cell_grid_with_given_dimensions() {
-        new Game(1, 1);
-    }
-
-    @Test
     public void initialise_cells_to_dead() {
         Cell cell = new Game(3, 5).cellAt(2, 3);
         assertFalse(cell.isAlive());
