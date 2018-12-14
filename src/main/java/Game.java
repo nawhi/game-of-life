243c567ec5544d12adc79a5912da2c8356f17f8d
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-14 17:09:07 +0000

    Simple Game constructor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
 public class Game {
 
+    public Game(int numRows, int numColumns) {
+
+    }
 }
--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class GameShould {
+
+    @Test
+    public void generate_cell_grid_with_given_dimensions() {
+        new Game(1, 1);
+    }
 }
