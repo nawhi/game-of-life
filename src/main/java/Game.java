3cfff2640ca9b311631da533bb7b688539218929
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-14 17:12:42 +0000

    Initialise gells grid in Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,17 @@
 public class Game {
 
-    public Game(int numRows, int numColumns) {
+    private final Cell[][] cells;
 
+    public Game(int numRows, int numColumns) {
+        cells = new Cell[numRows][numColumns];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
+                cells[i][j] = new GameCell(false);
+            }
+        }
+    }
+
+    public Cell cellAt(int row, int col) {
+        return cells[row][col];
     }
 }
--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class GameShould {
 
     @Test
     public void generate_cell_grid_with_given_dimensions() {
         new Game(1, 1);
+    }
+
+    @Test
+    public void initialise_cells_to_dead() {
+        Cell cell = new Game(1, 1).cellAt(0, 0);
+        assertFalse(cell.isAlive());
     }
 }
